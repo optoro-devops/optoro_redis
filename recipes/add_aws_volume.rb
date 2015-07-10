2c13932e42771d919543292ad4fd177431971a6c
Jeff Gillis <jgillis@optoro.com>
2015-07-10 12:38:42 -0400

    Add aws volume for redis datadir
    Add aws depenency
    Use correct hash
    Update Gemfile
    Add recursive setting to data directory creation
    Add test recipe for directory creation

---
+++ recipes/add_aws_volume.rb
@@ -0,0 +1,42 @@
+#<
+# This recipe creates an aws volume for the redis data directory
+#>
+
+# Attach redis data dir volume
+include_recipe 'aws'
+
+directory '/var/optoro/redis' do
+  owner 'redis'
+  group 'redis'
+  mode '0755'
+  action :create
+end
+
+aws_ebs_volume 'redis_ebs_volume' do
+  size 40
+  device '/dev/sdf'
+  action [:create, :attach]
+end
+
+# create a filesystem
+execute 'mkfs' do
+  command 'mkfs -t ext4 /dev/xvdf'
+  not_if 'mountpoint /var/optoro/redis'
+end
+
+directory '/var/optoro/redis' do
+  owner 'redis'
+  group 'redis'
+  mode '0755'
+  action :create
+end
+
+mount '/var/optoro/redis' do
+  device '/dev/xvdf'
+  fstype 'ext4'
+  options 'noatime,nobootwait'
+  action [:enable, :mount]
+  not_if 'mountpoint /var/optoro/redis'
+end
+
+node.set['redisio']['default_settings']['datadir'] = '/var/optoro/redis'
---
+++ recipes/test.rb
@@ -0,0 +1,4 @@
+directory '/var/optoro' do
+  action :create
+  mode '0755'
+end
