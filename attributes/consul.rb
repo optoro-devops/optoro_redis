71aba18cc423e428971e253b7132c100c04c5f74
Jeff Gillis <jgillis@optoro.com>
2015-08-18 11:42:59 -0400

    Tweak service settings

--- attributes/consul.rb
+++ attributes/consul.rb
@@ -1,4 +1,4 @@
 default['consul']['service_name'] = 'redis'
-default['consul']['config']['server'] = false
-default['consul']['config']['expect_boostrap'] = 0
+default['consul']['service']['server'] = false
+default['consul']['service']['expect_boostrap'] = 0
 
