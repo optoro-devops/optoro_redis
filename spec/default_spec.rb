95df1a94cd9e461692cae189ecc900485dd7587c
Scott Medefind <smedefind@optoro.com>
2015-12-01 15:02:25 -0500

    Removing optoro_metrics

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -16,7 +16,6 @@ describe 'optoro_redis::default' do
           redisio
           redisio::install
           redisio::enable
-          optoro_metrics::redis
         ).each do |recipe|
           it 'includes #{recipe}' do
             expect(chef_run).to include_recipe(recipe)
